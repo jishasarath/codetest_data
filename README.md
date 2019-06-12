bbc3e7fa3fb8783becfd5ebb25554823c6744551
Jisha Sarath <jishas2010@gmail.com>
2019-06-12 04:23:15 +0400

    codetest f
    test

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# codetest_data
+﻿
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge. 
+
+
+**To do these challenges, you need a basic understanding of HTML, CSS and (sometimes) JavaScript.**
+
+## Where to find everything
+
+Your task is to build out the project to the designs inside the `/design` folder. You will find both a mobile and a desktop version of the design to work to. 
+
+The designs are in JPG static format. The reason for this is to help train your eye when developing. This will mean that you'll need to use your best judgment for styles such as `font-size`, `padding` and `margin`. This should help when you're working on real projects, but do have access to full design files.
+
+You will find all the required assets in the `/images` folder. The assets are already optimized.
+
+There is also a `style-guide.md` file, which contains the information you'll need, such as color palette and fonts.
+
+## Building your project
+
+Practice using version control by creating your own Github repository for this challenge. This has the added benefit of being able to publish it on the `gh-pages` branch at the end. Find out more about Github Pages here: [https://pages.github.com/](https://pages.github.com/).
+
+
+## Suggested workflow
+
+Feel free to use any workflow that you feel comfortable with. Below is a suggested process, but do not feel like you need to follow these steps:
+
+1) Initialize your project as a public repository on [Github](https://github.com/). This will make it easier to share your code with the community if you need some help. If you're not sure how to do this, have a read through of this [Try Git](https://try.github.io/) resource.
+2) Look through the designs to start planning out how you'll tackle the project. This step is crucial to help you think ahead for CSS classes that you could create to make reusable styles.
+3) Before adding any styles, structure your content with HTML. Writing your HTML first can help focus your attention on creating well-structured content.
+4) Write out the base styles for your project, including general content styles, such as `font-family` and `font-size`.
+5) Start adding styles to the top of the page and work down. Only move on to the next section once you're happy you've completed the area you're working on.
+
---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"  crossorigin="anonymous">
+
+        <link href='https://fonts.google.com/specimen/Poppins' rel='stylesheet'>
+        <link href='https://fonts.google.com/specimen/Open+Sans' rel='stylesheet'>
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <style>
+
+/*            body {
+                font-family: 'Open Sans';font-weight: 400;
+            }*/
+
+            .header-text {
+                padding-top: 61px;
+                padding-left: 100px;
+                position:absolute; left:0; right:0;top:0
+            }
+            .head-typo {
+                font-family: 'Poppins',sans-serif;
+                color: #FFF;
+                font-size: 35px;
+                font-weight: bold;
+                padding-top: 33px;
+            }
+            .content-typo {
+                font-family: 'Open Sans',sans-serif;font-weight: 400;
+                color: #FFF;
+                line-height: 1;
+                font-size: 15px;
+            }
+
+            .rowstyler {
+                margin-top: 159px;
+                margin-left: -15px;
+
+            }
+
+            .banner {
+                display: inline;
+                -moz-box-sizing: border-box;
+                /*box-sizing: border-box;*/
+                background-size: cover;
+                width: 180px; /* Width of new image */
+                height: 236px; /* Height of new image */
+                padding-left: 180px; /* Equal to width of new image */
+            }
+        </style>
+        <title>Huddle</title>
+    </head>
+    <body style="background-color:  #674BAF;overflow: hidden;">
+        <div class="header-text">
+            <img src="images/logo.svg" alt="Image to show" style="width: 157px;" />
+        </div>
+        <div id="main-body"  style="background-image: url('images/bg-desktop.svg');z-index:100;background-repeat: no-repeat;
+             /* width: 1440px; */
+             height: 1000px;
+             position: absolute;
+             left: 112px;
+             top: 0px;
+             background-size: 945px 661px;" >
+
+
+
+            <div class="row rowstyler">
+                <div class="col-lg-6" style="    padding-left: 10px;padding-top: 10px;">
+                    <img src="images/illustration-mockups.svg" alt="Image to show" style="float: left;
+                         width: 607px;
+                         height: 417px;" />
+                </div>
+                <div class="col-lg-6" style="float:right;padding-right: 252px;">
+                    <div class="head-typo">
+                        Build The Community
+                        <p>
+                            Your Fans Will Love
+                    </div>
+                    <div class="content-typo">
+                        Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.
+                        <p></p>
+                        <button class="btn button-primary" style="border-radius: 32px; background-color: #FFF; width: 171px; color: #674BAF;" type="submit">Register</button>
+                    </div>
+                    <div class="row">
+
+                    </div>
+                </div>
+            </div>
+            <!--<div class="col-lg-12 text-center" style="float:right">-->
+
+            
+            <!--</div>-->
+        </div>
+
+<ul class="list-inline social-icon" style="float:right;padding-right:89px;padding-top:530px;">
+                 <li class="list-inline-item"><a href="#"><i class="fa fa-facebook" style="color:white; border-color: white;display: inline-block;border-radius: 60px;box-shadow: 0px 0px 2px #888;padding: 0.5em 0.6em;"></i></a>
+                </li>
+                <li class="list-inline-item"><a href="#"><i class="fa fa-twitter" style="color:white;border-color: white; display: inline-block;border-radius: 60px;box-shadow: 0px 0px 2px #888;padding: 0.5em 0.6em;"></i></a>
+                </li>                    
+               
+                <li class="list-inline-item"><a href="#"><i class="fa fa-instagram" style="color:white;border-color: white;display: inline-block;border-radius: 60px;box-shadow: 0px 0px 2px #888;padding: 0.5em 0.6em;"></i></a>
+                </li>
+            </ul>
+        
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"  crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"  crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"  crossorigin="anonymous"></script>
+    </body>
+</html>
